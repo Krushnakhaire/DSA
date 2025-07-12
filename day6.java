7760dcee9616fae8d2696f6baa10f20f11a70f1d
Krushnakhaire <krushnakhaire2004@gmail.com>
2025-07-12 17:56:41 +0530

    Add DSA - [day6]

---
+++ day6.java
@@ -0,0 +1,19 @@
+public class day6 {
+    public void  FabonacciRecursion(int i,int n){
+        if(n<=1){
+            return;
+        }
+        int last=n-1;
+        int secondlast=n-2;
+        int result=last+secondlast;
+        System.out.println(result);
+
+    }
+    public static void main(String args[]){
+        int n=5;
+        day6 d6 = new day6();
+        d6.FabonacciRecursion(0, n);
+        
+    }
+    
+}
