6300b5c3547a4438f98500e5efec72c5be1709e0
Krushnakhaire <krushnakhaire2004@gmail.com>
2025-10-07 21:37:42 +0530

    Your commit message here

---
+++ linkedList/LinklistBasics.java
@@ -0,0 +1,15 @@
+class LinklistBasics {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 5, 6, 8};
+    }
+}
